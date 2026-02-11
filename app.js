3cb2aebb5fddd75d1cf334f6953c9ea18a4f1eb6
Denzell Mackay <denzell.mackay@hotmail.com>
2026-02-11 12:00:56 -0500

    added seed data, access script created. Forgot to add defer to script tag(oops)

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+async function loadPlaces() {
+    const res = await fetch("./data/locationSeed.json");
+    if (!res.ok)
+        throw new Error(
+            `Failed to load JSON (${res.status} ${res.statusText}) at ./data/locationSeed.json`,
+        );
+    const places = await res.json();
+    return places;
+}
+
 function main(message) {
     document.getElementById("myEl").innerText = message;
 }
 
 main("Hello, World!");
+
+loadPlaces().then((places) => {
+    console.log(places[0].name);
+});
