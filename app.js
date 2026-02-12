75c841d30ca81dff66f9d39bf5fbfff1c09a27dd
Denzell <58393180+Denzell-M@users.noreply.github.com>
2026-02-11 19:00:23 -0500

    updated h1 tag to fit theme, added map element

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 async function loadPlaces() {
-    const res = await fetch("./data/locationSeed.json");
-    if (!res.ok)
-        throw new Error(
-            `Failed to load JSON (${res.status} ${res.statusText}) at ./data/locationSeed.json`,
-        );
-    const places = await res.json();
-    return places;
+  const res = await fetch("./data/locationSeed.json");
+  if (!res.ok) {
+    throw new Error(
+      `Failed to load JSON (${res.status} ${res.statusText}) at ./data/locationSeed.json`,
+    );
+  }
+  const places = await res.json();
+  return places;
 }
 
 async function loadSecret() {
-    const res = await fetch("./secret.json");
-    if (!res.ok)
-        throw new Error(
-            `Failed to load JSON (${res.status} ${res.statusText}) at ./secret.json`,
-        );
-    const secret = await res.json();
-    return secret;
+  const res = await fetch("./secret.json");
+  if (!res.ok) {
+    throw new Error(`
+      Failed to load JSON ((${res.status} ${res.statusText}) at ./secret.json`);
+  }
+  const secret = await res.json();
+  return secret;
 }
-
-function main(message) {
-    document.getElementById("myEl").innerText = message;
-}
-
-main("Hello, World!");
-
-loadPlaces().then((places) => {
-    console.log(places[0].name);
-});
 
 function loadGoogleMapsScript(apiKey) {
-    return new Promise((resolve, reject) => {
-        window.initMap = () => resolve();
+  return new Promise((resolve, reject) => {
+    window.initMap = () => resolve();
 
-        const script = document.createElement("script");
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap&libraries=places`;
-        script.async = true;
-        script.defer = true;
-        script.onerror = reject;
-        document.head.appendChild(script);
-    });
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?=${apiKey}&callback=initMap&libraries=places`;
+    script.async = true;
+    script.defer = true;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
+
+function assertMapEl() {
+  const mapEl = document.getElementByID("map");
+  if (!mapEl) {
+    throw new Error(`
+      Missing #map Element. Create element to continue.`);
+  }
+  return mapEl;
 }
